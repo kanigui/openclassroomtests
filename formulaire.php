c24221af1425aaee2a6fc71822f28955e693ea1a
SORO Kanigui Kolotioloma <kanigui_soro@yahoo.fr>
2018-05-07 16:56:53 +0000

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -2,6 +2,13 @@
 <html>
   <head>
       <title>Transmissions de données de page en page</title>
-      <>
+      <meta charset="utf-8" >
   </head>
+  <body>
+       <form method="post" action="cible.php">
+        <p>
+           On insèrera ici les éléments de notre formulaire.
+        </p>
+</form>
+  <body>
 </html>
